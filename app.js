77885781837a97588d6ad834adf65c47cf2352ca
Kishan <kishan.o@hotmail.com>
2019-09-05 21:45:00 +0100

    Set up express route for POST request from client side

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('Listening at 3000'));
-app.use(express.static('public'));
-
 const cheerio = require('cheerio');
 const request = require('request');
 const fs = require('fs');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+app.listen(3000, () => console.log('Listening at 3000'));
+app.use(express.static('public'));
+
 const writeStream = fs.createWriteStream('public/health-a-to-z.csv');
 
 //Generate csv file of all symptoms from NHS Health A-Z
@@ -16,8 +19,9 @@ function nhsScrape() {
 
 			const symptoms = $('.nhsuk-list-panel__item a').each((i, element) => {
 				const symptom = $(element).text();
+				const link = $(element).attr('href');
 
-				writeStream.write(`${symptom}, \n`);
+				writeStream.write(`${symptom},${link}\n`);
 			});
 
 			console.log('Scraping done.');
@@ -27,3 +31,15 @@ function nhsScrape() {
 
 nhsScrape();
 
+//Grab whatever condition the user entered from the client side 
+app.use(express.json({limit: '1mb'}));
+
+let condition;
+app.post('/api', (request, response) => {
+	console.log(request.body);
+	//Grab condition from the body and replace white spaces with hyphens
+	condition = request.body.link;
+	console.log(condition);
+	
+	response.end();
+});
