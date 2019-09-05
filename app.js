7f41601efe46ec98eb2bbe899ed36e7c7fd28ee9
Kishan <kishan.o@hotmail.com>
2019-09-05 17:53:55 +0100

    Web scraped NHS Health A-Z

--- app.js
+++ app.js
@@ -2,3 +2,28 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
+
+//Generate csv file of all symptoms from NHS Health A-Z
+const cheerio = require('cheerio');
+const request = require('request');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('public/health-a-to-z.csv');
+
+function nhsScrape() {
+	request('https://www.nhs.uk/conditions/', (error, response, html) => {
+		if (!error && response.statusCode == 200) {
+			const $ = cheerio.load(html);
+
+			const symptoms = $('.nhsuk-list-panel__item a').each((i, element) => {
+				const symptom = $(element).text();
+
+				writeStream.write(`${symptom}, \n`);
+			});
+
+			console.log('Scraping done.');
+		}
+	});
+}
+
+//nhsScrape(); --> Comment in line if health-a-to-z.csv file is not in public directory
+
